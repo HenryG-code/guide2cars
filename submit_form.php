9d2720d3d0a99da90c59a99cbeaf9bcd7ed97423
HenryG <gagiano18@gmail.com>
2024-12-18 20:42:45 +0200

    Website last touches

--- submit_form.php
+++ submit_form.php
@@ -1,9 +1,4 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -11,53 +6,41 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-session_start();
+$mail = new PHPMailer(true);
 
-// Initialize message variable
-$message_status = "";
+try {
+    // SMTP Server Settings
+    $mail->isSMTP();
+    $mail->Host = 'smtp.guide2cars.co.za'; // Correct SMTP server
+    $mail->SMTPAuth = true; // Enable authentication
+    $mail->Username = 'francois@guide2cars.co.za'; // Your full email address
+    $mail->Password = ''; // Your email password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Use SSL
+    $mail->Port = 465; // SMTP port for SSL
 
-// Process the form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve and sanitize inputs
-    $firstname = htmlspecialchars(trim($_POST['firstname']));
-    $surname = htmlspecialchars(trim($_POST['surname']));
-    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
-    $message = htmlspecialchars(trim($_POST['message']));
+    // Email Details
+    $mail->setFrom('francois@guide2cars.co.za', 'Guide2Cars Contact');
+    $mail->addAddress('francois@guide2cars.co.za'); // Recipient email
+    $mail->Subject = 'New Contact Form Submission';
+    
+    // Email Body Content
+    $firstname = htmlspecialchars($_POST['firstname']);
+    $surname = htmlspecialchars($_POST['surname']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $mail->Body = "You have received a new message from the contact form:\n\n" .
+                  "First Name: $firstname\n" .
+                  "Surname: $surname\n" .
+                  "Email: $email\n\n" .
+                  "Message:\n$message\n";
 
-    // Validate email address
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $message_status = "<p style='color: red;'>Invalid email address. Please enter a valid email.</p>";
-    } else {
-        try {
-            // Initialize PHPMailer
-            $mail = new PHPMailer(true);
-            $mail->isSMTP();
-            $mail->Host = 'smtp.xneelo.co.za'; // Xneelo SMTP server
-            $mail->SMTPAuth = true;
-            $mail->Username = 'francois@guide2cars.co.za'; // Your full email address
-            $mail->Password = '';       // Your email account password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use TLS encryption
-            $mail->Port = 465; // Port for TLS or 587
-
-            // Email settings
-            $mail->setFrom('francois@guide2cars.co.za', 'Guide2Cars');
-            $mail->addAddress('francois@guide2cars.co.za'); // Recipient email
-            $mail->addReplyTo($email, "$firstname $surname");
-
-            $mail->Subject = "New Contact Form Submission from $firstname $surname";
-            $mail->Body = "You have received a new message:\n\n" .
-                          "First Name: $firstname\n" .
-                          "Surname: $surname\n" .
-                          "Email: $email\n\n" .
-                          "Message:\n$message";
-
-            // Send the email
-            $mail->send();
-            $message_status = "<p style='color: green;'>Thank you, $firstname! Your message has been sent successfully.</p>";
-        } catch (Exception $e) {
-            $message_status = "<p style='color: red;'>Message could not be sent. Mailer Error: {$mail->ErrorInfo}</p>";
-        }
-    }
+    // Send Email
+    $mail->send();
+    echo "<p>Thank you! Your message has been sent successfully.</p>";
+} catch (Exception $e) {
+    echo "<p>Mailer Error: {$mail->ErrorInfo}</p>";
 }
 ?>
+
 
